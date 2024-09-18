fd13aa3ae1e4346c8a66261f0f73d604369e3933
Churchinruins <surhoisaev@gmail.com>
2024-09-19 01:19:58 +0300

    Create:Burger and Mobile V

--- js/main.js
+++ js/main.js
@@ -9,6 +9,28 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
 });
 
+
+const swiperNews = new Swiper('.swiperNews', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination-new',
+        bulletClass: '.swiper-pagination-newbullet'
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const burger = document.querySelector('.menu-burger__header')
+const menu = document.querySelector('.header__wrapper-mobile')
+function handleBurgerClick() {
+    menu.classList.toggle('header__wrapper-mobile_active')
+    burger.classList.toggle('menu-burger__header_active')
+}
+
+burger.addEventListener('click', handleBurgerClick)
